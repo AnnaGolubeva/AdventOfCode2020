750dbd140ea450fced50b4607ce1cdf608724caf
Anna Go <golubevanna@gmail.com>
2020-12-05 11:21:44 -0500

    added code for day1 and day2

---
+++ day1.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+with open('puzzle1_numbers.txt') as f: nums= f.read().splitlines()
+nums = list(map(int, nums)) 
+
+num_nums=len(nums)
+
+for i in range(num_nums):
+	for j in range(num_nums):
+		for k in range(num_nums):
+			sum_nums= nums[i]+nums[j]+nums[k]
+			if sum_nums==2020:
+				print(f'{nums[i]}*{nums[j]}*{nums[k]} = {nums[i]*nums[j]*nums[k]}')
+				break
+
+
+
+#=== from reddit ===#
+
+#=== PART 1
+# vals = map(int, nums)
+# while vals:
+#     x = vals.pop(0)
+#     [print(x*v) for v in vals if x+v==2020]
+
+
+
---
+++ day2.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+with open('puzzle2_input.txt') as f: inp= f.read().splitlines()
+#nums = list(map(int, nums))
+
+
+# PART 1
+# num_valid=0
+# print(len(inp))
+# for i in inp:
+# 	m, let, pw = i.split()
+# 	mi,ma = m.split("-")
+# 	mi=int(mi)
+# 	ma=int(ma)
+# 	let=let.strip(":")
+# 	num_occ= pw.count(let)
+
+# 	if num_occ>=mi:
+# 		if num_occ<=ma:
+# 			num_valid+=1
+
+# print(num_valid)
+
+
+# PART 2
+num_valid=0
+
+for i in inp:
+	m, let, pw = i.split()
+	mi,ma = m.split("-")
+	mi=int(mi)
+	ma=int(ma)
+	let=let.strip(":")
+	num_occ= pw.count(let)
+
+	if num_occ>=mi:
+		if num_occ<=ma:
+			num_valid+=1
+
+print(num_valid)
